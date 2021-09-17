5b3f26f3454d87fe8e6510a9479c91febef16d28
Victor-johnsson <70903378+Victor-johnsson@users.noreply.github.com>
2021-09-17 15:22:26 +0200

    Second Commit try

---
+++ src/main/java/com/example/ht21sysb13dbprogassignmtht21sysb13sqlservice/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.ht21sysb13dbprogassignmtht21sysb13sqlservice;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
