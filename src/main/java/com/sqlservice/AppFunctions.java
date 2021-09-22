ccf2e7cdc25d6adc283d3f7850f4c164dc77f9ec
Victor-johnsson <70903378+Victor-johnsson@users.noreply.github.com>
2021-09-22 12:44:35 +0200

    Gjort en randomCode generator!

--- src/main/java/com/sqlservice/AppFunctions.java
+++ src/main/java/com/sqlservice/AppFunctions.java
@@ -12,7 +12,9 @@ import javafx.util.Callback;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class AppFunctions {
     //samlar alla metoder som är universella som kan kalla på dem från alla andra klasser, generella metoder.
@@ -115,6 +117,22 @@ public class AppFunctions {
 
         tableView.setItems(filteredData);
         ContosoConnection.connectionClose(resultSet);
+    }
+
+    public static String randomCode(String dbTableName, String idColumnName, String startingLetter) throws SQLException{
+        DataAccessLayer dataAccessLayer = new DataAccessLayer();
+        ResultSet resultSet = dataAccessLayer.getAllFromTable(dbTableName);
+        while (true) {
+            ArrayList<String> arrayList = new ArrayList<>();
+            while (resultSet.next()){
+                arrayList.add(resultSet.getString(idColumnName));
+            }
+            int randomNum = ThreadLocalRandom.current().nextInt(10000, 99999);
+            String randomCode = startingLetter + randomNum;
+            if (!(arrayList.contains(randomCode))) {
+                return randomCode;
+            }
+        }
     }
 
 }
