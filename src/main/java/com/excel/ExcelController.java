aa68c2a92cd278ab27d21c08a795882e22a7d4b5
Victor-johnsson <70903378+Victor-johnsson@users.noreply.github.com>
2021-09-28 13:26:24 +0200

    ExcelBranch

---
+++ src/main/java/com/excel/ExcelController.java
@@ -0,0 +1,7 @@
+package com.excel;
+
+public class ExcelController {
+
+
+
+}
--- src/main/java/com/excel/HelloExcel.java
+++ src/main/java/com/excel/HelloExcel.java
@@ -1,26 +1,24 @@
-package com.sqlservice;
+package com.excel;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
-import java.sql.SQLException;
 
 
-public class HelloApplication extends Application {
+public class HelloExcel extends Application {
     //klassen som startar hela programmet
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("studentView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("src/main/java/com/excel/excelView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Contoso University Administrator");
         stage.setScene(scene);
         stage.show();
     }
-
-
 
     public static void main(String[] args) {
         launch();
